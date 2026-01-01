97140aa49590127d8f0ac25e821fb0419148eb9f
Arham Garg <arhamvgarg@gmail.com>
2026-01-01 19:11:33 +0530

    CF - 2130A

---
+++ Codeforces/2130A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+
+    long long sum = 0;
+    int cnt0 = 0;
+
+    for (int i = 0; i < n; i++) {
+      int x;
+      cin >> x;
+
+      sum += x;
+
+      if (x == 0) {
+        cnt0++;
+      }
+    }
+
+    cout << sum + cnt0 << "\n";
+  }
+
+  return 0;
+}
